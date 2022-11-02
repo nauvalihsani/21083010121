960030b3fc8a40cad909996dea86e40f50be5534
nauval <nihsaniazis08@gmail.com>
2022-11-02 17:02:43 +0700

    Pertemuan-1

---
+++ Tugas-1/Tugas-1.sh
@@ -0,0 +1,2 @@
+echo "halo dunia!"
+echo "ini adalah skrip Bash pertamaku di Linux"
---
+++ Tugas-1/tugas_1.py
@@ -0,0 +1,11 @@
+print("....................................")
+print("Tugas SISOP 1 Kelas B")
+print("....................................")
+print("Nama : Nauval Ihsani Azis")
+print("NPM  : 21083010121")
+print("...................................")
+print("Notes : haihai")
+print("halo dunia")
+print("ini adalah script bash pertamaku di linux")
+
+
